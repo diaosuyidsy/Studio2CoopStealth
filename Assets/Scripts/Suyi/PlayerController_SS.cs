eac934f9fb0a47696c70a1e44afa392dc8974d06
U-DESKTOP-11UIFRG\diaos <suyi.diao@vanderbilt.edu>
2019-02-14 17:46:06 -0500

    tested split Camera

--- Assets/Scripts/Suyi/PlayerController_SS.cs
+++ Assets/Scripts/Suyi/PlayerController_SS.cs
@@ -1,18 +1,44 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Rewired;
 
 public class PlayerController_SS : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	[Header("Player Controller is supposed to be either 0 or 1")]
+	public int PlayerID;
+	public float MoveSpeed = 10f;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	#region Player Controller Section
+	private Player _player;
+	private Rigidbody _rb;
+	private Vector3 _moveVector;
+	private bool _jump;
+	#endregion
+
+	private void Awake()
+	{
+		_player = ReInput.players.GetPlayer(PlayerID);
+		_rb = GetComponent<Rigidbody>();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		_getInput();
+		_processMovement();
+	}
+
+	private void _getInput()
+	{
+		_moveVector.x = _player.GetAxis("Move Horizontal") * MoveSpeed;
+		_moveVector.y = _rb.velocity.y;
+		_moveVector.z = _player.GetAxis("Move Vertical") * MoveSpeed;
+		_jump = _player.GetButton("Jump");
+	}
+
+	private void _processMovement()
+	{
+		_rb.velocity = _moveVector;
+	}
 }
