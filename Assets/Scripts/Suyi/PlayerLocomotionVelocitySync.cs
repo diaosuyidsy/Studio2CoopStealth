58792b5043a19321e7ce64b9ecd214061dd6271d
U-DESKTOP-11UIFRG\diaos <suyi.diao@vanderbilt.edu>
2019-02-24 15:13:43 -0500

    adding player

---
+++ Assets/Scripts/Suyi/PlayerLocomotionVelocitySync.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLocomotionVelocitySync : MonoBehaviour
+{
+	private Transform RootTransform;
+	private Rigidbody _rb;
+	private Animator _animator;
+	private Vector2 _smoothDeltaPosition = Vector2.zero;
+	Vector3 velocity = Vector2.zero;
+
+	private void Awake()
+	{
+		_animator = GetComponent<Animator>();
+		_rb = GetComponentInParent<Rigidbody>();
+		RootTransform = transform.parent;
+	}
+
+	private void Update()
+	{
+		ConsoleProDebug.Watch("Player Velocity", _rb.velocity.ToString());
+		velocity = _rb.velocity;
+		_animator.SetFloat("InputHorizontal", velocity.x);
+		_animator.SetFloat("InputVertical", velocity.z);
+		_animator.SetFloat("InputMagnitude", velocity.magnitude);
+	}
+
+}
