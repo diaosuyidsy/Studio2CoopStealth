49ae0ffcab0b1a0105bc7de6e0ceca913987190c
U-DESKTOP-11UIFRG\diaos <suyi.diao@vanderbilt.edu>
2019-03-14 00:23:43 -0400

    added single step on door

--- Assets/Scripts/Suyi/Puzzles/Interactable_StandOnDoorSingle.cs
+++ Assets/Scripts/Suyi/Puzzles/Interactable_StandOnDoorSingle.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Interactable_HoldOpenDoor : Interactable
+public class Interactable_StandOnDoorSingle : Interactable
 {
 	public Transform Door;
 	public Vector3 FinalPositionOffset = Vector3.one;
@@ -22,7 +22,11 @@ public class Interactable_HoldOpenDoor : Interactable
 
 	private void Update()
 	{
-		if (state == InteractableState.Idle)
+		if (state == InteractableState.Interacting)
+		{
+			OnInteracting();
+		}
+		else
 		{
 			Door.position = Vector3.MoveTowards(Door.transform.position, _DoorInitialPosition, Time.deltaTime * DoorCloseMovingSpeed);
 		}
@@ -31,5 +35,21 @@ public class Interactable_HoldOpenDoor : Interactable
 	public override void OnInteracting(Object param = null)
 	{
 		Door.position = Vector3.MoveTowards(Door.transform.position, _DoorFinalPosition, Time.deltaTime * DoorOpenMovingSpeed);
+	}
+
+	private void OnCollisionEnter(Collision collision)
+	{
+		if (collision.collider.CompareTag("Player1") || collision.collider.CompareTag("Player2"))
+		{
+			base.OnInteractDown();
+		}
+	}
+
+	private void OnCollisionExit(Collision collision)
+	{
+		if (collision.collider.CompareTag("Player1") || collision.collider.CompareTag("Player2"))
+		{
+			base.OnInteractUp();
+		}
 	}
 }
