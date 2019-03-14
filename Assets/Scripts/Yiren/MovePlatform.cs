034a470c431c7ad178dfd85387d1d42537552e5a
Yiren Wang <yw4062@nyu.edu>
2019-03-14 15:44:28 -0400

    Added Dotween and 2 levels

---
+++ Assets/Scripts/Yiren/MovePlatform.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+
+public class MovePlatform : MonoBehaviour
+{
+    private GameObject movePlatform;
+    // Start is called before the first frame update
+    void Start()
+    {
+        movePlatform = transform.parent.gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag.Equals("Player2"))
+        {
+            movePlatform.GetComponent<DOTweenAnimation>().DOPlayForwardById("Move");
+            other.transform.parent = movePlatform.transform;
+        }
+
+        if (other.name.Contains("Pillar"))
+        {
+            Destroy(movePlatform);
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag.Equals("Player2"))
+        {
+            movePlatform.GetComponent<DOTweenAnimation>().DOPause();
+            other.transform.parent = null;
+        }
+    }
+}
