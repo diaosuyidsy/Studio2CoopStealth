7d66c2c646fa059402bfe0cad51234cffdb72f7b
U-DESKTOP-11UIFRG\diaos <suyi.diao@vanderbilt.edu>
2019-04-01 20:43:23 -0400

    added level 1 to corridor 5

---
+++ Assets/Scripts/Suyi/DeathOnCollide.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathOnCollide : MonoBehaviour
+{
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.CompareTag("Enemy"))
+		{
+			other.gameObject.SetActive(false);
+		}
+	}
+}
