2293324dcfa3f6b30bb251a08c4c3dbbd244b276
Yiren Wang <yw4062@nyu.edu>
2019-05-09 10:28:48 -0400

    Finished new level scene

---
+++ Assets/Scripts/Yiren/Interactable_Elevator.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+
+public class Interactable_Elevator : MonoBehaviour
+{
+    public bool P1Entered;
+    public bool P2Entered;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player1")) P1Entered = true;
+        if (other.CompareTag("Player2")) P2Entered = true;
+        if (P1Entered && P2Entered)
+        {
+            GetComponent<DOTweenAnimation>().DOPlayBackwards();
+        }
+        
+        
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player1")) P1Entered = false;
+        if (other.CompareTag("Player2")) P2Entered = false;
+        
+    }
+}
