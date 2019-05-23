2a2073f510e1af3aafe040535b0f5e7fffa66045
U-DESKTOP-11UIFRG\diaos <suyi.diao@vanderbilt.edu>
2019-05-23 14:25:47 -0400

    added controller input scene load

--- Assets/Scripts/Yiren/LoadScene1.cs
+++ Assets/Scripts/Yiren/LoadScene1.cs
@@ -2,23 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using Rewired;
 
 public class LoadScene1 : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	// Start is called before the first frame update
+	void Start()
+	{
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	}
 
-    public void LoadFirstScene()
-    {
-        SceneManager.LoadScene(1);
-    }
+	// Update is called once per frame
+	void Update()
+	{
+		if (ReInput.players.GetPlayer(0).GetButtonUp("A") || ReInput.players.GetPlayer(1).GetButtonUp("A"))
+			SceneManager.LoadScene(1);
+	}
+
+	public void LoadFirstScene()
+	{
+		SceneManager.LoadScene(1);
+	}
 }
